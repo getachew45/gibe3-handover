f3f01da30b3800cbdfbbab9465849aa545472228
getachew45 <getachewtenaw3@gmail.com>
2026-06-09 20:46:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const SUPABASE_URL =
+"https://aggxeaxapxvdnleenkvk.supabase.co/rest/v1/";
+const SUPABASE_KEY =
+"YOUR_ANON_PUBLIC_KEY";
+const supabaseClient =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
+);
+const saveBtn =
+document.getElementById("saveBtn");
+saveBtn.addEventListener(
+"click",
+async () => {
+alert("Save button works");
+
+});
